c30e74b5274c48d8307ddf6f33a36443c9062aa5
nicol <nicolaspancini436@gmail.com>
2025-11-25 12:43:18 -0400

    pequenas_alterações

--- tarefas.py
+++ tarefas.py
@@ -31,6 +31,8 @@ if opcao == 1:
                 print('LIMITE DE 3 TENTATIVAS ALCANÇADO!')
                 sleep(2)
                 exit()
+    print(f'\033[1;33mBem-vindo {nome_user}\033[m')
+    sleep(1)
 # Cadastro
 cadastrado = False
 if opcao == 2:
@@ -73,43 +75,49 @@ try:
 except FileNotFoundError:
     with open(ARQUIVO, "x", encoding="utf-8") as arquivo:
         Tarefas = []
-print('\033[1;35m-_\033[m'* 10)
-print('\033[1;36mTo Do List\033[m')
-print('\033[1;35m-_\033[m'* 10)
+print('-' * 40)
+print('| \033[1;34mTO DO LIST\033[m |'.center(50, '-'))
 escolha = -1
 while escolha != 0:
     try:
-        escolha = int(input('[1] Adicionar tarefa\n[2] Remover tarefa\n[3] Ver lista\n[0] Salvar\nR: '))
+        escolha = int(input('\033[1;34m[1]\033[m Adicionar tarefa\n\033[1;34m[2]\033[m Remover tarefa\n\033[1;34m[3]\033[m Ver lista\n\033[1;34m[0]\033[m Salvar\nR: '))
         if escolha < 0 or escolha > 4:
             print('Erro: o número deve estar entre \033[1;97m0\033[m e \033[1;97m3\033[m')
         else:
             if escolha == 1:
-                tarefa = str(input('Tarefa que será adicionada: ').strip().title())
+                tarefa = str(input('\033[1;34mTarefa que será adicionada:\033[m ').strip().title())
                 if tarefa in Tarefas:
                     print(f'A tarefa \033[1;32m{tarefa}\033[m já existe.')
                 else:
                     Tarefas.append(tarefa)
                     print(f'Tarefa \033[1;32m{tarefa}\033[m foi adicionada!')
             if escolha == 2:
-                tarefa = str(input('Tarefa que deseja remover: ').strip().title())
-                if tarefa not in Tarefas:
-                    print(f'A tarefa \033[1;32m{tarefa}\033[m não existe.')
+                if len(Tarefas) > 0:
+                    print('\033[1;97m| TAREFAS |\033[m'.center(50, '-'))
+                    for pos, tar in enumerate(Tarefas):
+                        print(f'\033[1;33m{pos+1}\033[m • {Tarefas[pos]}')
+                    indice = int(input('\033[1;34mQual o número da tarefa que deseja remover?\033[m\nR: '))
+                    if Tarefas[indice-1] not in Tarefas:
+                        print(f'A tarefa de número\033[1;32m{indice}\033[m não existe.')
+                    else:
+                        print(f'Tarefa \033[1;32m{Tarefas[indice-1]}\033[m foi removida!')
+                        Tarefas.remove(Tarefas[indice-1])
                 else:
-                    Tarefas.remove(tarefa)
-                    print(f'Tarefa \033[1;32m{tarefa}\033[m foi removida!')
+                    print('Não há tarefas no momento.')
             if escolha == 3:
                 print(f'\033[1;97mLista de tarefas:\033[m')
                 if len(Tarefas) == 0:
-                    print('Nenhuma tarefa adicionada até o momento.')
+                    print('\033[1;37mNenhuma tarefa adicionada até o momento.\033[m')
                 else:
                     qntd = 0
                     for l in range(0, len(Tarefas)):
                         qntd += 1
-                        print(f'{qntd} • {Tarefas[l]}')
+                        print(f'\033[1;33m{qntd}\033[m • {Tarefas[l]}')
             if escolha == 0:
-                print('Salvando...')
+                print('\033[1;33mSalvando...\033[m')
     except ValueError:
         print('\033[1;31mErro:\033[m Tente novamente.')
+    print('\033[1;97m-\033[m' * 50)
     sleep(2)
 # adiciona o que estiver dentro dele no documento.
 with open(ARQUIVO, 'w', encoding='utf-8') as arquivo:
